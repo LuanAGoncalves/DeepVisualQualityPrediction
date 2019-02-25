90fd04bcb1fdb61de405e4627525cc415ee79eb8
LuanAGoncalves <eng.luangonsalves@gmail.com>
2019-02-25 17:31:30 -0300

    Version 2.1.3

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ def saveChekpoint(
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find("Conv") != -1:
-        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+        torch.nn.init.xavier_uniform_(m.weight.data)
     elif classname.find("BatchNorm") != -1:
         torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
         torch.nn.init.constant_(m.bias.data, 0)
